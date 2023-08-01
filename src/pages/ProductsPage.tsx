5ace3d7b70bc2c96b6e7554ec279d7ce9395fbf4
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-08-01 15:37:53 +0000

    start react navigation

---
+++ src/pages/ProductsPage.tsx
@@ -0,0 +1,12 @@
+import { products } from "../data/products";
+import React from "react";
+
+export function ProductsPage() {
+   return (
+      <div className="text-center p-5">
+         <h2 className="text-x1 font-bold text-slate-600">
+            Here are some great tools for React
+         </h2>
+      </div>
+   );
+}
